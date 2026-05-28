dcf85ec4761c76832376f4653d28242684218e18
Federico Ponzi <me@fponzi.me>
2026-05-28 22:36:25 +0100

    Update copyright

--- src/test/java/me/fponzi/tink/azurekv/AzureKeyVaultAeadTest.java
+++ src/test/java/me/fponzi/tink/azurekv/AzureKeyVaultAeadTest.java
@@ -1,4 +1,4 @@
-// Copyright 2024 Federico Ponzi
+// Copyright 2026 Federico Ponzi
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
